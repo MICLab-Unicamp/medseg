5bc5251a0b0cca9ca778a877acf191ea6722de30
dscarmo <carmodiedre@outlook.com>
2023-05-15 19:30:37 -0300

    fix monitoring bug

--- medseg/monitoring.py
+++ medseg/monitoring.py
@@ -3,12 +3,20 @@ import GPUtil
 
 
 def get_stats():
-    stats = {}
-    gpu = GPUtil.getGPUs()[0]
-    stats["gpu"] = int(gpu.load*100)
-    stats["gpu_ram"] = int(gpu.memoryUtil*100)
-    stats["cpu"] = int(psutil.cpu_percent())
-    stats["cpu_ram"] = int(psutil.virtual_memory().percent)
+    stats = {"gpu": 0, "gpu_ram": 0, "cpu": 0, "cpu_ram": 0}
+    try:
+        gpu = GPUtil.getGPUs()[0]
+        stats["gpu"] = int(gpu.load*100)
+        stats["gpu_ram"] = int(gpu.memoryUtil*100)
+    except Exception as e:
+        print(f"GPU Monitoring error: {e}")
+
+    try:
+        stats["cpu"] = int(psutil.cpu_percent())
+        stats["cpu_ram"] = int(psutil.virtual_memory().percent)
+    except Exception as e:
+        print(f"CPU Monitoring error: {e}")
+        
     return stats
 
 if __name__ == "__main__":
